e9765afc9f2bcd62bfb5e6dfad7aea1bab4cc263
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-10-01 12:21:17 +0530

    done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,7 +11,6 @@ import { useNavigate } from "react-router-dom";
 const BACKEND_URL = "https://edc-pict.site";
 
 export default function Login() {
-  const [adminId, setAdminId] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
