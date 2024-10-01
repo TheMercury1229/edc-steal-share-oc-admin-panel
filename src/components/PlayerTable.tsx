1bd0815be700f2c0b915c21e56787002e17b59dc
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-10-01 11:04:00 +0530

    done

--- src/components/PlayerTable.tsx
+++ src/components/PlayerTable.tsx
@@ -45,7 +45,7 @@ export default function PlayersPage() {
     try {
       const response = await axios.get(`${BACKEND_URL}/admin/get-all-players`, {
         headers: {
-          Authorization: localStorage.getItem("token"), // Send the token in the Authorization header
+          token: localStorage.getItem("token"), // Send the token in the Authorization header
         },
         withCredentials: true, // Include cookies if necessary
       });
